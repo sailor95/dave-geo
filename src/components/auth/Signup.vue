45c80053a1314956158f8c9e6e9ec2a873dd6875
David Huang <bugman195@gmail.com>
2020-10-26 14:05:47 +0800

    Add Signup comp

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="signup container">
+    <form @submit.prevent="signup" class="card-panel">
+      <h2 class="center deep-purple-text">Signup</h2>
+      <div class="field">
+        <label for="email">Email:</label>
+        <input type="email" name="email" v-model="email" />
+      </div>
+      <div class="field">
+        <label for="password">Password:</label>
+        <input type="password" name="password" v-model="password" />
+      </div>
+      <div class="field">
+        <label for="alias">Alias:</label>
+        <input type="text" name="alias" v-model="alias" />
+      </div>
+      <p class="red-text center" v-if="feedback">{{ feedback }}</p>
+      <div class="field center">
+        <button class="btn deep-purple">Signup</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+  data() {
+    return {
+      email: null,
+      password: null,
+      alias: null,
+      feedback: null,
+    };
+  },
+  methods: {
+    signup() {
+      if (this.alias) {
+        this.feedback = '';
+      } else {
+        this.feedback = 'Must enter alias';
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.signup {
+  max-width: 400px;
+  margin-top: 60px;
+}
+.signup h2 {
+  font-size: 2.4em;
+}
+
+.signup .field {
+  margin-bottom: 16px;
+}
+</style>
