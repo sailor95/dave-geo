b131a2db3b20efaea8c9ae384352defb2c0d114e
buugman195 <bugman195@gmail.com>
2020-11-05 00:18:51 +0800

    Add geo update to db process

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from 'firebase';
+import db from '@/firebase/init';
 
 export default {
   name: 'GMap',
@@ -27,8 +28,44 @@ export default {
     },
   },
   mounted() {
-    this.renderMap();
-    console.log(firebase.auth().currentUser);
+    let user = firebase.auth().currentUser;
+
+    // get user geolocation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        pos => {
+          this.lat = pos.coords.latitude;
+          this.lng = pos.coords.longitude;
+
+          // find the user record and update geo coodrs
+          db.collection('users')
+            .where('user_id', '==', user.uid)
+            .get()
+            .then(snapshot => {
+              snapshot.forEach(doc => {
+                db.collection('users')
+                  .doc(doc.id)
+                  .update({
+                    geolocation: {
+                      lat: pos.coords.latitude,
+                      lng: pos.coords.longitude,
+                    },
+                  });
+              });
+            })
+            .then(() => {
+              this.renderMap();
+            });
+        },
+        err => {
+          console.log(err);
+          this.renderMap();
+        },
+        { maximumAge: 60000, timeout: 3000 }
+      );
+    } else {
+      this.renderMap();
+    }
   },
 };
 </script>
