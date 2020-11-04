0b8fb14b8e1e56afa6848ebddb4e37b179c76dc0
buugman195 <bugman195@gmail.com>
2020-11-04 22:57:04 +0800

    Add router-link & wrap firebase auth

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -2,10 +2,14 @@
   <div class="navbar">
     <nav class="deep-purple darken-1">
       <div class="container">
-        <a href="" class="brand-logo">Ninja Geo</a>
+        <!-- <a href="" class="brand-logo">Ninja Geo</a> -->
+        <router-link class="brand-logo" :to="{ name: 'GMap' }"
+          >Ninja Geo</router-link
+        >
         <ul class="right">
-          <li><a href="">Signup</a></li>
-          <li><a href="">Login</a></li>
+          <li><router-link :to="{ name: 'Signup' }">Signup</router-link></li>
+          <li><router-link :to="{ name: 'Login' }">Login</router-link></li>
+          <li><a @click="logout">Logout</a></li>
         </ul>
       </div>
     </nav>
@@ -13,10 +17,22 @@
 </template>
 
 <script>
+import firebase from 'firebase';
+
 export default {
   name: 'Navbar',
   data() {
     return {};
+  },
+  methods: {
+    logout() {
+      firebase
+        .auth()
+        .signOut()
+        .then(() => {
+          this.$router.push({ name: 'Signup' });
+        });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import firebase from 'firebase';
 
-Vue.config.productionTip = false
+import App from './App';
+import router from './router';
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+Vue.config.productionTip = false;
+
+let app = null;
+
+// wait for firebase auth to init before creating the app
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      router,
+      components: { App },
+      template: '<App/>',
+    });
+  }
+});
