858d258de8f58de5a95b5c1f868601f3d2b48ab7
buugman195 <bugman195@gmail.com>
2020-11-04 17:59:52 +0800

    Connect signup with firestore auth

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -25,6 +25,7 @@
 <script>
 import slugify from 'slugify';
 import db from '@/firebase/init';
+import firebase from 'firebase';
 
 export default {
   name: 'Signup',
@@ -39,7 +40,7 @@ export default {
   },
   methods: {
     signup() {
-      if (this.alias) {
+      if (this.alias && this.email && this.password) {
         this.slug = slugify(this.alias, {
           replacement: '-',
           remove: /[$*_+~.()'"!\-:@]/g,
@@ -51,13 +52,29 @@ export default {
           if (doc.exists) {
             this.feedback = 'This alias already exists';
           } else {
+            firebase
+              .auth()
+              .createUserWithEmailAndPassword(this.email, this.password)
+              .then(cred => {
+                ref.set({
+                  alias: this.alias,
+                  geolocation: null,
+                  user_id: cred.user.uid,
+                });
+              })
+              .then(() => this.$router.push({ name: 'GMap' }))
+              .catch(err => {
+                console.log(err);
+                this.feedback = err.message;
+              });
+
             this.feedback = 'This alias is free to use';
           }
         });
 
         this.feedback = '';
       } else {
-        this.feedback = 'Must enter alias';
+        this.feedback = 'Must enter all fields';
       }
     },
   },
