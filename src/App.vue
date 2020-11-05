a2d9c14d05d02100bd03ae7d8fea8ee2fa645adb
buugman195 <bugman195@gmail.com>
2020-11-05 22:38:01 +0800

    Setup realtime chat comments

--- src/App.vue
+++ src/App.vue
@@ -16,4 +16,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+body {
+  background: #ddd;
+}
+</style>
--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -3,7 +3,10 @@
     <div v-if="profile" class="card">
       <h2 class="deep-purple-text center">{{ profile.alias }}'s Wall</h2>
       <ul class="comments collection">
-        <li>Comment</li>
+        <li v-for="(comment, index) in comments" :key="index">
+          <div class="deep-purple-text">{{ comment.from }}</div>
+          <div class="grey-text text-darken-2">{{ comment.content }}</div>
+        </li>
       </ul>
       <form @submit.prevent="addComment">
         <div class="field">
@@ -28,11 +31,13 @@ export default {
       newComment: null,
       feedback: null,
       user: null,
+      comments: [],
     };
   },
   created() {
     let ref = db.collection('users');
 
+    // get current user
     ref
       .where('user_id', '==', firebase.auth().currentUser.uid)
       .get()
@@ -43,11 +48,26 @@ export default {
         });
       });
 
+    // get profile data
     ref
       .doc(this.$route.params.id)
       .get()
       .then(user => {
         this.profile = user.data();
+      });
+
+    // comments
+    db.collection('comments')
+      .where('to', '==', this.$route.params.id)
+      .onSnapshot(snap => {
+        snap.docChanges().forEach(change => {
+          if (change.type == 'added') {
+            this.comments.push({
+              from: change.doc.data().from,
+              content: change.doc.data().content,
+            });
+          }
+        });
       });
   },
   methods: {
@@ -57,7 +77,7 @@ export default {
         db.collection('comments')
           .add({
             to: this.$route.params.id,
-            from: this.user.id,
+            from: this.user.alias,
             content: this.newComment,
             time: Date.now(),
           })
@@ -73,3 +93,18 @@ export default {
   },
 };
 </script>
+
+<style>
+.view-profile .card {
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2 {
+  font-size: 2em;
+  margin-top: 0;
+}
+.view-profile li {
+  padding: 10px;
+  border-bottom: 1px solid #eee;
+}
+</style>
