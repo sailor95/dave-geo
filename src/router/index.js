8508924fc44b34a5143650f16e6e4ee16e360b2d
buugman195 <bugman195@gmail.com>
2020-11-05 21:54:36 +0800

    Add route guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,23 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import firebase from 'firebase';
+
 import GMap from '@/components/home/GMap';
 import Signup from '@/components/auth/Signup';
 import Login from '@/components/auth/Login';
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   routes: [
     {
       path: '/',
       name: 'GMap',
       component: GMap,
+      meta: {
+        requiresAuth: true,
+      },
     },
     {
       path: '/signup',
@@ -26,3 +31,20 @@ export default new Router({
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  // Check require auth
+  if (to.matched.some(rec => rec.meta.requiresAuth)) {
+    let user = firebase.auth().currentUser;
+
+    if (user) {
+      next();
+    } else {
+      next({ name: 'Login' });
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
